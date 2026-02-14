bb88a8286e6f3fb22e294adb6e621b973376ba19
RoboMico <robomico@outlook.com>
2026-02-14 20:59:20 +0800

    chore: debug build script

--- tools/build.sh
+++ tools/build.sh
@@ -2,7 +2,16 @@
 #
 # For Cloudflare Workers building the site
 
+echo "[/home/robomico/constructor] Installing NPM Packages..."
 npm i
+
+echo "[/home/robomico/constructor] Installing bundle..."
 bundle install
+
+echo "[/home/robomico/constructor] Building JS scripts..."
 npm run build
+
+echo "[/home/robomico/constructor] Building website..."
 JEKYLL_ENV=production bundle exec jekyll build
+
+echo "[/home/robomico/constructor] Done!"
